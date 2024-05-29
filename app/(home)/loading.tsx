d5f21bee504e950493c38c3647461facaedad52e
loeybho <byo_l@naver.com>
2024-05-29 12:18:40 +0900

    Cont. Detail에 들어갈 데이터 컴포넌트 분리

---
+++ app/(home)/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <h2>Loading중 입니다</h2>;
+}
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,20 @@
-export default function MovieDetail({
+import MovieInfo from "@/components/movie-info";
+import MovieVideos from "@/components/movie-videos";
+import { Suspense } from "react";
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Moive {id}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie video</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }

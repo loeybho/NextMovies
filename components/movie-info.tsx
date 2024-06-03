761e6e137d56fccc28505b10e487ebaa332900dd
loeybho <byo_l@naver.com>
2024-06-03 11:27:05 +0900

    Feat. Detail에 동적 title 추가

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,12 +1,11 @@
 import { API_URL } from "@/app/(home)/page";
 import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id: string) {
-  console.log(`Fetching movies : ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
+
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
   return (

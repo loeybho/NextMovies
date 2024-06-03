3170bd5ff9fa8bdb45d6409b6c6212fc48a89a19
loeybho <byo_l@naver.com>
2024-06-03 11:55:16 +0900

    Cont. 홈에서 타입 지정

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -14,7 +14,7 @@ export default async function MovieVideos({ id }: { id: string }) {
   return (
     <div className={styles.container}>
       <h6>
-        {videos.map((video) => (
+        {videos.map((video: any) => (
           <iframe
             key={video.id}
             src={`https://youtube.com/embed/${video.key}`}
